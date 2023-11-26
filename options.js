877df8285b3ff99f44daa72277cb5bdbe7f074a6
akisatoon <akisatoon@gmail.com>
2023-11-26 14:03:07 +0900

    beta

--- options.js
+++ options.js
@@ -1,15 +1,40 @@
 "use strict";
 
+// userDataを表に追加
+function addUserData(key, item) {
+
+    // ユーザーデータを代入
+    const [videoID, videoTitle, speed, note] = [key, item.videoTitle, item.speed, item.note]
+
+    const theadEle = document.querySelector("table#userData");
+
+    const tbodyEle = document.createElement("tbody");
+
+    tbodyEle.innerHTML = `<th class="videoID">${videoID}</th>
+    <td class="videoTitle">${videoTitle}</td>
+    <td class="speed">${speed}</td>
+    <td class="note">${note}</td>`;
+
+    theadEle.appendChild(tbodyEle);
+}
+
 if (document.readyState === "loading") {
+
     // Loading hasn't finished yet
     document.addEventListener("DOMContentLoaded", () => {
+
         chrome.storage.sync.get(null, (items) => {
-            //for (key of Object.keys(items)) 
+
+            for (let key of Object.keys(items)) {
+
+                const item = items[key];
+
+                addUserData(key, item);
+            }
 
             // debug用
             // クラウドデータ
             console.log(items);
-
         });
 
         // debug用
@@ -22,4 +47,25 @@ if (document.readyState === "loading") {
     });
 } else {
 
+    chrome.storage.sync.get(null, (items) => {
+
+        for (let key of Object.keys(items)) {
+
+            const item = items[key];
+
+            addUserData(key, item);
+        }
+
+        // debug用
+        // クラウドデータ
+        console.log(items);
+    });
+
+    // debug用
+    chrome.storage.sync.getBytesInUse(null, (bytesInUse) => {
+
+        // 使っているバイト数
+        console.log(bytesInUse);
+
+    });
 }
